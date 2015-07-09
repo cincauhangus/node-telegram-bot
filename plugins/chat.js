32e0d05395435958ce3b777e31d14666057920e3
Aaron Ho <aaron@alt.my>
2015-07-09 23:42:29 +0800

    cosmetic changes to default plugins

--- plugins/chat.js
+++ plugins/chat.js
@@ -1,36 +1,46 @@
 var util = require('util');
 var plugin = function () {
     var me = this;
-    
     this.init = function (bot) {
         me.botIdentity = bot._self;
     };
-    
+
     this.regex = [
         /\/(hello)/,
         /\/(boobs)/,
-        /\/(butts)/
+        /\/(butts)/,
+        /\/(spam)/,
+        /\/(cake)/
     ];
-    
+
     this.regexHandler = function (matches, msg, client) {
         if (!util.isArray(matches)) {
             return;
         }
-        
+
         var command = matches[1].toUpperCase();
-        if (STRINGS[command]) {
+        if (command.match(/spam/i)) {
+            var occurance = getRandomInt(3,20);
+            for (var i=0;i<occurance;i++) {
+                sendPrivateReply(STRINGS.SPAM(msg), msg, client);
+            }
+        } else  if (STRINGS[command]) {
             sendGroupReply(STRINGS.get(command, msg, me.botIdentity, client), msg, client);
         }
     };
-    
-    var sendGroupReply = function (response, msg, client) {
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function sendGroupReply(response, msg, client) {
         client.sendMessage({
             chat_id: msg.chat.id,
             text: response
         });
     };
-    
-    var sendPrivateReply = function (response, msg, client) {
+
+    function sendPrivateReply(response, msg, client) {
         client.sendMessage({
             chat_id: msg.from.id,
             text: response
@@ -51,6 +61,12 @@ var STRINGS = new (function () {
     this.BUTTS = function (msg) {
         return util.format("No butts for you @%s!", msg.from.username);
     };
+    this.CAKE = function (msg) {
+        return "The cake is a lie";
+    };
+    this.SPAM = function (msg) {
+        return "STOP SPAMMING ME /spam@"+msg.from.username;
+    }
     this.DEFAULT = function (msg, bot) { return ""; };
 })();
 
--- plugins/test.js
+++ plugins/test.js
@@ -1,10 +1,10 @@
 var plugin = function () {
     this.init = function (bot) {
-        console.log("Plugin initialization completed.");
+        console.log("TEST: Plugin initialization completed.");
     };
     this.regex = [/\/test/];
     this.regexHandler = function (matches, msg, bot) {
-        console.log("Handling regex match.");
+        console.log("TEST: Handling regex match.");
         bot.sendMessage({
             chat_id: msg.from.id,
             text: "Test is successful"
